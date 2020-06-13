98c2bfed557681524e6795702d1783c4fd7511d7
Angelos Petropoulos <angelpe@microsoft.com>
2020-06-13 02:35:42 -0500

    Add project files.

---
+++ Models/Books.cs
@@ -0,0 +1,30 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Example_Cosmos_DB_MongoDB_API.Models
+{
+    public class Book
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        #region snippet_BookNameProperty
+        [BsonElement("Name")]
+        [JsonProperty("Name")]
+        public string BookName { get; set; }
+        #endregion
+
+        public decimal Price { get; set; }
+
+        public string Category { get; set; }
+
+        public string Author { get; set; }
+    }
+
+}
